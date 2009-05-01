a1c779fc22f835c0a81f28c3b160b962495b4c1c
Lev Givon <lev@columbia.edu>
2009-05-01 16:52:18 -0400

    Remove iaf_decode_ins and iaf_decode_rec for now because they do not perform well.

--- Contents.m
+++ Contents.m
@@ -2,23 +2,22 @@
 % Version 0.01
 %
 % Asynchronous Sigma-Delta Modulator Functions
-%   asdm_decode     - ASDM time decoding machine.
-%   asdm_decode_ins - Parameter-insensitive ASDM time decoding machine.
-%   asdm_encode     - ASDM time encoding machine.
+%   asdm_decode      - ASDM time decoding machine.
+%   asdm_decode_fast - Fast ASDM time decoding machine.
+%   asdm_decode_ins  - Parameter-insensitive ASDM time decoding machine.
+%   asdm_encode      - ASDM time encoding machine.
 %
 % Integrate-and-Fire Functions
-%   iaf_decode      - IAF time decoding machine.
-%   iaf_decode_fast - Fast IAF time decoding machine.
-%   iaf_decode_ins  - Parameter-insensitive IAF time decoding machine.
-%   iaf_decode_rec  - Recursive IAF time decoding machine.
-%   iaf_encode      - IAF time encoding machine.
+%   iaf_decode       - IAF time decoding machine.
+%   iaf_decode_fast  - Fast IAF time decoding machine.
+%   iaf_encode       - IAF time encoding machine.
 %
 % Utility Functions
-%   gen_test_signal - Generate bandlimited test signals.
+%   gen_test_signal  - Generate bandlimited test signals.
 %
 % Demonstrations
-%   asdm_demo       - Demos that use the ASDM encoder/decoders.
-%   iaf_demo        - Demos that use the IAF encoder/decoders.
+%   asdm_demo        - Demos that use the ASDM encoder/decoders.
+%   iaf_demo         - Demos that use the IAF encoder/decoders.
 
 % Author(s): Lev Givon
 % Copyright 2009 Trustees of Columbia University
