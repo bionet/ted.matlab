10f9337b7e1a7d9e48fe679cedddfb20ca40386c
Lev Givon <lev@columbia.edu>
2010-01-06 18:26:07 -0500

    Fix typo and handle negative values.

--- ted/si.m
+++ ted/si.m
@@ -1,12 +1,12 @@
 %SI Sine integral function.
-%  Y = SI(X) computes the sine integral INT(SIN(T)/T,T,0,X).
-%  This implementation is faster than Matlab's SININT function
+%   Y = SI(X) computes the sine integral INT(SIN(T)/T,T,0,X).
+%   This implementation is faster than Matlab's SININT function
 %
-%  See also SININT.
+%   See also SININT.
 
-%  Adapted from specfun (http://www.netlib.org/specfun/)
-%  by Lev Givon
-%  Copyright 2009 Trustees of Columbia University
+%   Adapted from specfun (http://www.netlib.org/specfun/)
+%   by Lev Givon
+%   Copyright 2009 Trustees of Columbia University
 
 function y = si(x)
 
@@ -14,6 +14,12 @@ y = arrayfun(@si_scalar, x);
 
 function y = si_scalar(x);
 
+if x < 0,
+    sgn = -1;
+    x = -x;
+else
+    sgn = 1;
+end
 x2 = x*x;
 if x == 0,
     y = 0.0;
@@ -22,7 +28,7 @@ else
         y = (((((3.1e-7)*x2-2.834e-5)*x2+1.66667e-3)*x2-5.555556e-2)*x2+1.0)*x;
     else
         fx = ((((x2+38.027264)*x2+265.187033)*x2+335.67732)*x2+38.102495)/ ...
-             ((((x2+40.021433)*x2+322.62491)*x2+570.23628)*x2+ ...
+             ((((x2+40.021433)*x2+322.624911)*x2+570.23628)*x2+ ...
               157.105423);
         gx = ((((x2+42.242855)*x2+302.757865)*x2+352.018498)*x2+21.821899)/ ...
              ((((x2+48.196927)*x2+482.485984)*x2+1114.978885)*x2+ ...
@@ -30,3 +36,4 @@ else
         y = 1.570796327-fx*cos(x)/x-gx*sin(x)/x;
     end
 end
+y = y*sgn;
