3eae3b8b8bb86b87e4eab766b56c56ed823358dd
Lev Givon <lev@columbia.edu>
2010-01-07 10:40:24 -0500

    Reformat si.m.

--- ted/si.m
+++ ted/si.m
@@ -1,6 +1,7 @@
 %SI Sine integral function.
-%   Y = SI(X) computes the sine integral INT(SIN(T)/T,T,0,X).
-%   This implementation is faster than Matlab's SININT function
+%   Y = SI(X) computes the sine integral INT(SIN(T)/T,T,0,X) using
+%   a rational approximation. This implementation is faster than 
+%   Matlab's SININT function.
 %
 %   See also SININT.
 
@@ -14,26 +15,21 @@ y = arrayfun(@si_scalar, x);
 
 function y = si_scalar(x);
 
-if x < 0,
-    sgn = -1;
-    x = -x;
-else
-    sgn = 1;
-end
+sgn = sign(x);
+x = abs(x);
+
 x2 = x*x;
 if x == 0,
-    y = 0.0;
+    si = 0.0;
+elseif x <= 1.0,
+    si = (((((3.1e-7)*x2-2.834e-5)*x2+1.66667e-3)*x2-5.555556e-2)*x2+1.0)*x;
 else
-    if x <= 1.0,
-        y = (((((3.1e-7)*x2-2.834e-5)*x2+1.66667e-3)*x2-5.555556e-2)*x2+1.0)*x;
-    else
-        fx = ((((x2+38.027264)*x2+265.187033)*x2+335.67732)*x2+38.102495)/ ...
-             ((((x2+40.021433)*x2+322.624911)*x2+570.23628)*x2+ ...
-              157.105423);
-        gx = ((((x2+42.242855)*x2+302.757865)*x2+352.018498)*x2+21.821899)/ ...
-             ((((x2+48.196927)*x2+482.485984)*x2+1114.978885)*x2+ ...
-              449.690326)/x;
-        y = 1.570796327-fx*cos(x)/x-gx*sin(x)/x;
-    end
+    fx = ((((x2+38.027264)*x2+265.187033)*x2+335.67732)*x2+38.102495)/ ...
+         ((((x2+40.021433)*x2+322.624911)*x2+570.23628)*x2+ ...
+          157.105423);
+    gx = ((((x2+42.242855)*x2+302.757865)*x2+352.018498)*x2+21.821899)/ ...
+         ((((x2+48.196927)*x2+482.485984)*x2+1114.978885)*x2+ ...
+          449.690326)/x;
+    si = 1.570796327-fx*cos(x)/x-gx*sin(x)/x;
 end
-y = y*sgn;
+y = si*sgn;
