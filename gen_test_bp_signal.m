c96eda3522fb4bf6053558942e3ece1fff7aa7cf
Lev Givon <lev@columbia.edu>
2010-03-01 12:43:31 -0500

    Tidy up function header, use built-in randi function.

--- gen_test_bp_signal.m
+++ gen_test_bp_signal.m
@@ -1,20 +1,20 @@
 %GEN_TEST_BP_SIGNAL Generate bandlimited, uniformly sampled test signal
-%   X = GEN_TEST_BP_SIGNAL(DUR,DT,FMIN,FMAX) returns a signal DUR seconds
-%   long uniformly sampled at intervals DT. The generated signal's 
-%   frequency components are no lower than FMIN Hz and no higher than FMAX Hz.
+%   X = GEN_TEST_BP_SIGNAL(DUR,DT,FMIN,FMAX) returns a signal DUR
+%   seconds long uniformly sampled at intervals DT. The generated
+%   signal's frequency components are no lower than FMIN Hz and no
+%   higher than FMAX Hz.
 %
-%   X = GEN_TEST_BP_SIGNAL(DUR,DT,FMIN,FMAX,NP) adds Gaussian white noise
-%   with power NP (in dB) to the generated signal before filtering
-%   it. If NP is -inf, no noise will be added.
+%   X = GEN_TEST_BP_SIGNAL(DUR,DT,FMIN,FMAX,NP) adds Gaussian white
+%   noise with power NP (in dB) to the generated signal before
+%   filtering it. If NP is -inf, no noise will be added.
 %
 %   X = GEN_TEST_BP_SIGNAL(DUR,DT,FMIN,FMAX,NP,NC) sets the number of
 %   frequency components in the generated signal to NC (the default is
-%   3). If NC is 0, the generated signal will either be zero if NP
-%   is -inf or pure filtered Gaussian white noise otherwise.
+%   3). If NC is 0, the generated signal will either be zero if NP is
+%   -inf or filtered pure Gaussian white noise otherwise.
 
-%   Author(s): Eftychios A. Pnevmatikakis (based on gen_test_signal written
-%       by Lev Givon)
-%   Copyright 2009 Trustees of Columbia University
+%   Authors: Lev Givon and Eftychios A. Pnevmatikakis 
+%   Copyright 2009-2010 Trustees of Columbia University
 
 function u = gen_test_bp_signal(dur,dt,fmin,fmax,varargin)
 
@@ -50,26 +50,25 @@ n = round(dur/dt);
 f = zeros(1,n);
 fmaxi = floor(n*fmax/fs);
 fmini = ceil(n*fmin/fs);
-if fmaxi - fmini -1 < nc,
+if fmaxi-fmini-1 < nc,
   error('frequency support is too narrow to provide %i frequency components',nc);
 end
 
 ci = [];
 while length(ci) < nc,
-  temp = randint(fmini+2,fmaxi+1);
+  temp = randi([fmini+2,fmaxi+1]);
   while intersect(ci,temp),
-    temp = randint(fmini+2,fmaxi+1);
+    temp = randi([fmini+2,fmaxi+1]);
   end
   ci = [ci, temp];
 end
 p = -pi*rand(1,nc);
 f(ci) = (n/2)*exp(1j*p);
-%f(end-ci+2) = (n/2)*exp(1j*-p);
 
 % Create the signal by transforming the constructed frequency 
 % representation into the time domain and adding white noise if so
 % specified:
-u = real(ifft(f)) + randn(1,n)*10^(np/20);
+u = real(ifft(f))+randn(1,n)*10^(np/20);
 
 % Filter the result. Since a cutoff of 1 corresponds to the Nyquist
 % frequency 1/(2*dt), the cutoff corresponding to the frequency
@@ -77,12 +76,3 @@ u = real(ifft(f)) + randn(1,n)*10^(np/20);
 b = fir1(40,[2*fmin*dt,2*fmax*dt]);
 u = filter(b,1,u);
 u = u(1:n);
-
-% Generate a random integer in the range [a,b):
-function r = randint(a,b)
-
-if a >= b,
-  error('maximum bound must exceed minimum bound');
-end
-r = a + floor((b-a)*rand());
-
