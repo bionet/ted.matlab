9a9b04bdc3bdf17254feb2ffda71e90f8324c26d
Lev Givon <lev@columbia.edu>
2010-03-01 12:39:21 -0500

    Make function header conform to Matlab conventions, toss unused code.

--- fftconv.m
+++ fftconv.m
@@ -1,24 +1,12 @@
-function y = fftconv(x,h,mode)
-%y = fftconv(x,h,mode)
-%fft convolution is much faster than overlap-add
-%   x,h the signals to be convolved
-%   mode = 1 zero pads h at the tail end (default)
-%   mode = 2 zero pads h symmetrically
+%FFTCONV Convolve two signals using FFT.
+%   Y = FFTCONV(A,B) convolves vectors A and B. It uses the FFT to
+%   accelerate the computation.
 
-if exist('mode') ~= 1
-    mode = 1;
-end
+%   Authors: Eftychios A. Pnevmatikakis and Robert J. Turetsky
+%   Copyright 2009-2010 Trustees of Columbia University
 
-lx = length(x);
-lh = length(h);
+function y = fftconv(a,b)
 
-if mode == 2
-    y = real(ifft(fft([x zeros(1,lh-1)]).*fft([zeros(1,floor(lx/2)) h zeros(1,ceil(lx/2)-1)])));
-else
-    y = real(ifft(fft([x zeros(1,lh-1)]).*fft([h zeros(1,lx-1)])));
-end
+y = real(ifft(fft([a zeros(1,length(b)-1)]).* ...
+              fft([b zeros(1,length(a)-1)])));
 
-%if any(isnan(y))
-%    'Reverting to manual convolution... this might take a while.'
-%    y = conv(x,h);
-%end
