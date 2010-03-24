2e5fe4a8a10970a1b2bd35893d7b7f0b2218c51a
Lev Givon <lev@columbia.edu>
2010-03-24 10:59:10 -0400

    Add fftconv and gen_test_bp_signal to the toolbox.

--- utils/Contents.m
+++ utils/Contents.m
@@ -1,13 +1,20 @@
 % Bionet Matlab Utilities
 % Version 0.014
 %
-% Utility Functions
-%   fig_resize      - Resize a figure.
-%   func_timer      - Time the execution of a function.
-%   gen_test_signal - Generate bandlimited test signals.
-%   plot_compare    - Compare two signals graphically.
-%   plot_encoded    - Plot a time-encoded signal.
-%   plot_signal     - Plot a sampled signal.
+% Signal Generation Functions
+%   gen_test_bp_signal - Generate bandpass test signals.
+%   gen_test_signal    - Generate bandlimited test signals.
+%
+% Visualization Functions
+%   fig_resize         - Resize a figure.
+%   plot_compare       - Compare two signals graphically.
+%   plot_encoded       - Plot a time-encoded signal.
+%   plot_raster        - Plot raster of time sequences.
+%   plot_signal        - Plot a sampled signal.
+%
+% Other Functions
+%   fftconv            - Convolve two signals using the FFT.
+%   func_timer         - Time the execution of a function.
 
 % Author: Lev Givon
 % Copyright 2009-2010 Trustees of Columbia University
